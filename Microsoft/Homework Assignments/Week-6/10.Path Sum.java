a63e12729169674af0697c74d7ebb03f84191434
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 14:58:18 +0530

    Create 10.Path Sum.java

---
+++ Microsoft/Homework Assignments/Week-6/10.Path Sum.java
@@ -0,0 +1,15 @@
+public boolean hasPathSum(TreeNode root, int targetSum) {
+		return helper(root, targetSum);
+	}
+
+private boolean helper(TreeNode root, int targetSum){
+	if(root == null){
+		return false;
+	}
+	targetSum -= root.val;
+	if(targetSum == 0 && root.left == null && root.right == null){
+		return true;
+	}
+
+	return helper(root.left, targetSum) || helper(root.right, targetSum);
+}
