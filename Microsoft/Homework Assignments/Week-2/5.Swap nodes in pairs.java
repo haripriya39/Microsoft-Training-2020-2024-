682e492cae0bb51451498e5377a5fa63ce4f102f
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:10:48 +0530

    Update 5.Swap nodes in pairs.java

--- Microsoft/Homework Assignments/Week-2/5.Swap nodes in pairs.java
+++ Microsoft/Homework Assignments/Week-2/5.Swap nodes in pairs.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode t = head;
+        while(t != null && t.next != null){
+            int v = t.next.val;
+            t.next.val = t.val;
+            t.val = v;
+            t = t.next.next;
+        }
+        return head;
+    }
+}
