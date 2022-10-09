1fb430e5f984223cec2ec21c5a0518d579b8430f
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:01:52 +0530

    Update 4.Valid Sudoku.java

--- Microsoft/Homework Assignments/Week-1/4.Valid Sudoku.java
+++ Microsoft/Homework Assignments/Week-1/4.Valid Sudoku.java
@@ -1,1 +1,31 @@
+public class Solution {
+public static boolean isValidSudoku(char[][] board) {
+		Set<Character>[] setXX = new HashSet[9];
+		Set<Character>[] setYY = new HashSet[9];
+		Set<Character>[] setXY = new HashSet[9];
+		for (int i = 0; i < 9; i++) {
+			setXX[i] = new HashSet<>();
+			setYY[i] = new HashSet<>();
+			setXY[i] = new HashSet<>();
+		}
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (board[i][j] == '.') {
+					continue;
+				}
+				if(!setXX[i].add(board[i][j])){
+					return false;
+				}
+				if(!setYY[j].add(board[i][j])){
+					return false;
+				}
+				if(!setXY[(j / 3)* 3 + i / 3].add(board[i][j])){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 
+}
