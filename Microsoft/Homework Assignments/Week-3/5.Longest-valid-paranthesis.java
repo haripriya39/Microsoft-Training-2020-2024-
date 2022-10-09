65c5c896890cd5f023ca222df3c1eac71d81eefc
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:15:38 +0530

    Update 5.Longest-valid-paranthesis.java

--- Microsoft/Homework Assignments/Week-3/5.Longest-valid-paranthesis.java
+++ Microsoft/Homework Assignments/Week-3/5.Longest-valid-paranthesis.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1); 
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            char curr = s.charAt(i);
+            if(curr == '('){
+                stack.push(i);
+            }else{
+                if(!stack.isEmpty() && stack.peek() >= 0 && s.charAt(stack.peek()) == '('){
+                    stack.pop();
+                    int last = stack.peek(); 
+                    result = Math.max(result, i - last);
+                }else{
+                    stack.push(i);
+                }
+            }
+        }
+        return result;
+    }
+}
