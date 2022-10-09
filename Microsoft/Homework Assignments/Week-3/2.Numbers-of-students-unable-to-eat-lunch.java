9d95b6fd0f02e997142c594b04627c0e3a3d0dc2
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:13:09 +0530

    Update 2.Numbers-of-students-unable-to-eat-lunch.java

--- Microsoft/Homework Assignments/Week-3/2.Numbers-of-students-unable-to-eat-lunch.java
+++ Microsoft/Homework Assignments/Week-3/2.Numbers-of-students-unable-to-eat-lunch.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int student1 = 0, student0 = 0;
+        for (int student: students) {
+            if (student == 1) {
+                ++student1;
+            } else {
+                ++student0;
+            }
+        }
+        for (int i = 0; i < sandwiches.length; ++i) {
+            if (sandwiches[i] == 1 && student1 == 0) {
+                return student0;
+            } else if (sandwiches[i] == 0 && student0 == 0){
+                return student1;
+            }
+            if (sandwiches[i] == 1) {
+                --student1;
+            } else {
+                --student0;
+            }            
+        }
+        return 0;
+    }
+}
