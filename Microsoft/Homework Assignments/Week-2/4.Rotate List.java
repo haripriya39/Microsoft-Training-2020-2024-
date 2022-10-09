da183bae90f574e4a9d2b9fb3a8b9c5c0fc9d2a4
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:09:54 +0530

    Update 4.Rotate List.java

--- Microsoft/Homework Assignments/Week-2/4.Rotate List.java
+++ Microsoft/Homework Assignments/Week-2/4.Rotate List.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null) return head;
+        
+        int len = 1;
+        ListNode newHead = head;
+        ListNode tail = head;
+        
+        while(tail.next != null) {
+            tail = tail.next;
+            len++;
+        }
+        tail.next = head;
+       
+        k = k % len;
+       
+        for(int i=0; i<len-k; i++)
+            tail = tail.next;
+            newHead = tail.next;
+            tail.next = null;
+            return newHead;
+    }
+}
