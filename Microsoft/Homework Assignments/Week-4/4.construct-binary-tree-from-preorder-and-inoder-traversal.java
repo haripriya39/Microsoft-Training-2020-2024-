35d7ab48ef8433d384ba2c9b0ad8afe869036261
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:17:44 +0530

    Update 4.construct-binary-tree-from-preorder-and-inoder-traversal.java

--- Microsoft/Homework Assignments/Week-4/4.construct-binary-tree-from-preorder-and-inoder-traversal.java
+++ Microsoft/Homework Assignments/Week-4/4.construct-binary-tree-from-preorder-and-inoder-traversal.java
@@ -1,1 +1,25 @@
-
+class Solution {
+    int preindx=0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+       
+        TreeNode ans=solve(preorder,inorder,0,inorder.length-1);
+        return ans;
+    }
+    public TreeNode solve(int[] preorder, int[] inorder,int s,int e){
+        if (s>e){
+            return null;
+        }
+        int ele=preorder[preindx++];
+       TreeNode temp=new TreeNode(ele);
+        int pos=find(inorder,ele,s,e);
+        temp.left=solve(preorder,inorder,s,pos-1);
+        temp.right=solve(preorder,inorder,pos+1,e);
+        return temp;
+    }
+    public int find(int inorder[],int ele,int s,int e){
+        for(int i=s; i<=e; i++){
+            if(ele==inorder[i])
+                return i;        }
+        return -1;
+    }
+}
