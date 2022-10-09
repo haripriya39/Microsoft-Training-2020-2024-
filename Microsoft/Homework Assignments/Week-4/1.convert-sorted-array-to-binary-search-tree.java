f5e09bf2d6261c6e788dea4f9726e84abd009bac
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:16:12 +0530

    Update 1.convert-sorted-array-to-binary-search-tree.java

--- Microsoft/Homework Assignments/Week-4/1.convert-sorted-array-to-binary-search-tree.java
+++ Microsoft/Homework Assignments/Week-4/1.convert-sorted-array-to-binary-search-tree.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums,0,nums.length-1);
+    }
+    public TreeNode dfs(int[] nums, int left, int right){
+        if(left>right){
+            return null;
+        }
+        int mid=(left+right)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left=dfs(nums,left,mid-1);
+        root.right=dfs(nums,mid+1,right);
+        return root;
+    }
+}
