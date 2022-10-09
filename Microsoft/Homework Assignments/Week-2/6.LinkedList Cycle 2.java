a8625055f7911894d8a17f6601ae3b17ae5e94c8
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:11:27 +0530

    Update 6.LinkedList Cycle 2.java

--- Microsoft/Homework Assignments/Week-2/6.LinkedList Cycle 2.java
+++ Microsoft/Homework Assignments/Week-2/6.LinkedList Cycle 2.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head==null)
+            return null;
+        HashSet<ListNode> hset = new HashSet<>();
+        ListNode temp = head;
+        while(temp.next!=null)
+        {
+            if(hset.contains(temp))
+                return temp;
+            else
+            {
+                hset.add(temp);
+                temp = temp.next;
+            }
+        }
+        return null;
+    }
+}
