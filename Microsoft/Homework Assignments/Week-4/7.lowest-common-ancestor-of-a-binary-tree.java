5f57898d77e3ca2b079c34d288257b619fa2559f
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:19:43 +0530

    Update 7.lowest-common-ancestor-of-a-binary-tree.java

--- Microsoft/Homework Assignments/Week-4/7.lowest-common-ancestor-of-a-binary-tree.java
+++ Microsoft/Homework Assignments/Week-4/7.lowest-common-ancestor-of-a-binary-tree.java
@@ -1,1 +1,36 @@
+class Solution {
+    TreeNode treeFound;
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    found(root,p,q);
+    return treeFound;
+        
+    }
+    
+    public boolean[] found(TreeNode root, TreeNode p, TreeNode q){
+     boolean[] pq =new boolean[2];
+        if(treeFound!=null){
+            return pq;
+        }
+        if(root==null){
+            return pq;
+        }
+        if(root==p){
+            pq[0]=true;
+        }
+        if(root==q){
+            pq[1]=true;
+        }
+
+        boolean[] x= found(root.left,p,q);
+        boolean[] y=found(root.right,p,q);
+        pq[0]= x[0]||y[0]||pq[0];
+        pq[1]= x[1]||y[1]||pq[1];
+        if(pq[0] && pq[1] && treeFound==null){
+        
+        treeFound= root;}
+
+        return pq;
+        
+    }
+}
