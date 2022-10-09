ca67789b9b95e679797cb821c5fe3a3253f30bcf
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:09:01 +0530

    Update 3.Remove Nth from end of List.java

--- Microsoft/Homework Assignments/Week-2/3.Remove Nth from end of List.java
+++ Microsoft/Homework Assignments/Week-2/3.Remove Nth from end of List.java
@@ -1,1 +1,10 @@
-
+class Solution {
+private int nthNode=0;
+public ListNode removeNthFromEnd(ListNode head, int n) {
+if(head==null) return head;
+head.next = removeNthFromEnd(head.next, n);
+nthNode++;
+if(nthNode==n) return head.next;
+return head;
+}
+}
