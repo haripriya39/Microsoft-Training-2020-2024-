743c77b31399bce3e97b770935ef05919853338e
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:07:09 +0530

    Update 1.Remove Duplicates from Sorted List.java

--- Microsoft/Homework Assignments/Week-2/1.Remove Duplicates from Sorted List.java
+++ Microsoft/Homework Assignments/Week-2/1.Remove Duplicates from Sorted List.java
@@ -1,1 +1,14 @@
-
+class Solution {
+public ListNode deleteDuplicates(ListNode head) {
+ListNode curr=head;
+while(curr!=null && curr.next!=null)
+{
+while(curr!=null && curr.next!=null && curr.val==curr.next.val)
+{
+curr.next=curr.next.next;
+}
+curr=curr.next;
+}
+return head;
+}
+}
