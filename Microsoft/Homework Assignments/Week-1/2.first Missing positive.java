b566180f2ae4ffac851d5395ad945821dd812742
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-09 15:00:05 +0530

    Update 2.first Missing positive.java

--- Microsoft/Homework Assignments/Week-1/2.first Missing positive.java
+++ Microsoft/Homework Assignments/Week-1/2.first Missing positive.java
@@ -1,1 +1,18 @@
-
+class Solution{
+public int firstMissingPositive(int[] nums) {
+    int start = 0;
+    int end = nums.length - 1;
+    while (start <= end) {
+        int index = nums[start] - 1;
+        if (index == start)
+            start++;
+        else if (index < 0 || index > end || nums[start] == nums[index])
+            nums[start] = nums[end--];
+        else {
+            nums[start] = nums[index];
+            nums[index] = index + 1;
+        }
+    }
+    return start + 1;
+}
+}
