90828939e2b4d8dd6f297e70d700c7bb28dfe010
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-29 12:39:07 +0530

    Create 06.Perfect Squares.java

---
+++ Microsoft/Homework Assignments/Week-9/06.Perfect Squares.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int numSquares(int n) {
+        int res = n, num =2;
+        while(num * num <=n){
+            int x = n/(num*num), y = n%(num*num);
+            res = Math.min(res, x + numSquares(y));
+            ++num;
+        }
+
+        return res;
+    }
+}
