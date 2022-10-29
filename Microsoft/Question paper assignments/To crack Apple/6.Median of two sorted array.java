53afc1ef03c63cdef1fe1b27e3a92e0411363881
haripriya39 <108563328+haripriya39@users.noreply.github.com>
2022-10-29 10:37:00 +0530

    Create 6.Median of two sorted array.java

---
+++ Microsoft/Question paper assignments/To crack Apple/6.Median of two sorted array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int arr[]=IntStream.concat(Arrays.stream(nums1),Arrays.stream(nums2)).sorted().toArray();
+int a=arr.length;
+if(a%2==0) return (double)(arr[a/2-1]+arr[a/2])/2;
+else return (double)arr[(a+1)/2-1];
+    }
+}
